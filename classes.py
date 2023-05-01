71cd0711bf942fbc0480f2198a89fc5c14fa5ff0
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-01 23:11:18 +0200

    set scraper and files

---
+++ classes.py
@@ -0,0 +1,8 @@
+items_container = { 'data-cy' : 'search.listing.organic'}
+container_li_tag_css_class = 'css-iq9jxc e1n6ljqa1'
+
+location_css_class = 'css-14aokuk e1n6ljqa7'
+item_description_css_class = 'e1n6ljqa19 css-6vtodn ei6hyam0'
+
+pagination_button_css_class = 'eo9qioj1 css-ehn1gc e1e6gtx31'
+
