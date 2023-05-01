4d4bf37798094a88042a79f5cf8ac326b4a8f564
Krzysztof Puchała <95151687+asyncWebDev@users.noreply.github.com>
2023-05-01 23:30:15 +0200

    Delete classes.py

--- classes.py
+++
@@ -1,8 +0,0 @@
-items_container = { 'data-cy' : 'search.listing.organic'}
-container_li_tag_css_class = 'css-iq9jxc e1n6ljqa1'
-
-location_css_class = 'css-14aokuk e1n6ljqa7'
-item_description_css_class = 'e1n6ljqa19 css-6vtodn ei6hyam0'
-
-pagination_button_css_class = 'eo9qioj1 css-ehn1gc e1e6gtx31'
-
